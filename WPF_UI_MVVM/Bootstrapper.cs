b3bb1f85577624d2c0c3de372ab18ad746df4092
aastha.gupta <gupta2020aastha@gmail.com>
2024-03-05 16:34:19 +0530

    Add project files.

---
+++ WPF_UI_MVVM/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_UI_MVVM.ViewModels;
+
+namespace WPF_UI_MVVM
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+
+            //Displaying ShellViewModel instead of MainWindow.xaml that is default StartUri in App.xaml
+            DisplayRootViewForAsync<ShellViewModel>();
+
+
+        }
+    }
+}
---
+++ WPF_UI_MVVM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_UI_MVVM.Models;
+
+namespace WPF_UI_MVVM.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+		private string _firstName = "Aastha";
+		private string _lastName = "Gupta";
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel> ();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Aastha", LastName = "Gupta" });
+            People.Add(new PersonModel { FirstName = "Shiv", LastName = "Rajput" });
+            People.Add(new PersonModel { FirstName = "Karan", LastName = "Singh" });
+            People.Add(new PersonModel { FirstName = "Reena", LastName = "Gill" });
+        }
+
+        public string FirstName
+		{
+			get 
+			{ 
+				return _firstName; 
+			}
+			set 
+			{ 
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { 
+				_lastName = value; 
+				NotifyOfPropertyChange(() =>  LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+		}
+
+		public string FullName
+		{
+			get 
+			{ 
+				return $"{FirstName} {LastName}"; 
+			}
+		}
+
+		public BindableCollection<PersonModel> People
+		{
+			get { return _people; }
+			set { _people = value; }
+		}
+
+		public PersonModel SelectedPerson
+        {
+			get { return _selectedPerson; }
+			set { 
+				_selectedPerson = value; 
+				NotifyOfPropertyChange(() => SelectedPerson);
+			}
+		}
+
+		public bool CanClearText(string firstName, string lastName)
+		{
+			return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+		}
+
+		public void ClearText(string firstName, string lastName)
+		{
+			FirstName = "";
+			LastName = "";
+		}
+
+		public void LoadPageOne()
+		{
+			ActivateItemAsync(new FirstChildViewModel());
+		}
+
+		public void LoadPageTwo()
+		{
+			ActivateItemAsync(new SecondChildViewModel());
+		}
+
+	}
+}
